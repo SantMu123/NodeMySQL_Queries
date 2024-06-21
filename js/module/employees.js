8d28234eba553effb42bf05d70349c2de2b18283
SantMu123 <smunozcastaneda14@gmail.com>
2024-06-21 17:19:55 -0500

    feat: :sparkles: nuevas consultas hechas

--- js/module/employees.js
+++ js/module/employees.js
@@ -41,4 +41,9 @@ export const getQuantityEmployeesByJobTitle = async()=>{
     return result;
 }
 
+//Una Tabla 2. Encontrar todos los empleados que trabajan en la oficina de San Francisco
+export const getAllEmployeesWithOfficeInSanFrancisco = async()=>{
+    let [result] = await connection.query(` SELECT * FROM employees WHERE officeCode = 1;`);
+    return result;
+}
 
